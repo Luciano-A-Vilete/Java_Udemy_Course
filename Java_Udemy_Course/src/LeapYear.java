c04fdcb2106fc36b4f9475c0459fb29265539cc6
Luciano Armando Vilete <50681089+Luciano-A-Vilete@users.noreply.github.com>
2023-06-06 12:28:18 -0300

    Delete Java_Udemy_Course directory
    deletion of duplicate archives

--- Java_Udemy_Course/src/LeapYear.java
+++
@@ -1,7 +0,0 @@
-public class LeapYear {
-    public static void main(String[] args) {
-
-    }
-        public static boolean isLeapYear(int year){
-            return year >= 1 && year <= 9999 && (year % 4 == 0 && year % 100 != 0 || year % 400 == 0);
-        }}
